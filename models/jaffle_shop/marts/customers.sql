2dab044e8a24e1601e911d7862724d7464a7a46e
Sam Debruyn <sam@debruyn.dev>
2023-06-07 23:30:04 +0200

    update readme

--- models/jaffle_shop/marts/customers.sql
+++ models/jaffle_shop/marts/customers.sql
@@ -18,7 +18,6 @@ order_summary as (
         customer_id,
 
         count(*) as count_lifetime_orders,
-        count(*) > 1 as is_repeat_buyer,
         min(ordered_at) as first_ordered_at,
         max(ordered_at) as last_ordered_at,
 
@@ -26,7 +25,7 @@ order_summary as (
         sum(order_total) as lifetime_spend
 
     from orders_mart
-    group by 1
+    group by customer_id
 
 ),
 
@@ -38,12 +37,7 @@ joined as (
         order_summary.first_ordered_at,
         order_summary.last_ordered_at,
         order_summary.lifetime_spend_pretax,
-        order_summary.lifetime_spend,
-
-        case
-            when order_summary.is_repeat_buyer then 'returning'
-            else 'new'
-        end as customer_type
+        order_summary.lifetime_spend
 
     from customers
 
