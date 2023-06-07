53f1e36d85957189e312f99f2f760888aaf1b9f8
Sam Debruyn <sam@debruyn.dev>
2023-06-07 20:06:40 +0200

    add nyt

--- models/new_york_taxi/staging/stg_green.sql
+++ models/new_york_taxi/staging/stg_green.sql
@@ -1,12 +1,2 @@
-with src as (
-    select *
-    from {{ source('new_york_taxi', 'green') }}
-),
-
-final as (
-    select *
-    from src
-)
-
 select *
-from final
+from {{ source('new_york_taxi', 'green') }}
--- models/new_york_taxi/staging/stg_yellow.sql
+++ models/new_york_taxi/staging/stg_yellow.sql
@@ -1,12 +1,2 @@
-with src as (
-    select *
-    from {{ source('new_york_taxi', 'yellow') }}
-),
-
-final as (
-    select *
-    from src
-)
-
 select *
-from final
+from {{ source('new_york_taxi', 'yellow') }}
